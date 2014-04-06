712df62696c65d47667172c86055350c61b5b7b2
crutchy <jared.crutchfield@hotmail.com>
2014-04-06 11:08:53 +1000

    first commit

---
+++ mother_repeat.php
@@ -0,0 +1,19 @@
+<?php
+
+# gpl2
+# by crutchy
+# 6-april-2014
+
+if ($argc<3)
+{
+  echo "error 1";
+  return;
+}
+if ($argv[1]<>"irc")
+{
+  echo "error 2";
+  return;
+}
+echo unserialize($argv[2]);
+
+?>
