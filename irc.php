3fdf7266977bbe26126b7fbf70fe94579f7536c2
crutchy <jared.crutchfield@hotmail.com>
2014-04-15 19:18:28 +1000

    script executive demo added

--- irc.php
+++ irc.php
@@ -2,24 +2,55 @@
 
 # gpl2
 # by crutchy
-# 9-april-2014
+# 15-april-2014
 
-$pwd=file_get_contents("test");
-define("NICK","bacon");
-define("PASSWORD",$pwd);
-unset($pwd);
+define("NICK","bacon"); # bacon/coffee/mother
+define("PASSWORD",file_get_contents("test"));
 define("LOG_FILE","log");
-define("CMD_QUIT","~Q");
-#define("CHAN_LIST","#test,##");
-define("CHAN_LIST","#~");
-define("CHAN_TERM","#~");
+define("EXEC_FILE","exec");
+define("EXEC_DELIM","/");
+define("TERM_PRIVMSG","privmsg");
+define("CMD_ABOUT","~");
+define("CMD_QUIT","~q");
+define("CMD_ADDEXEC","~add");
+define("CHAN_LIST","#test,#sublight");
+define("VALID_CHARS","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 .,#");
+define("TEMPLATE_DELIM","%%");
+define("TEMPLATE_MSG","msg");
+define("TEMPLATE_NICK","nick");
+define("TEMPLATE_CHAN","chan");
 set_time_limit(0);
 ini_set("display_errors","on");
+$admin_nicks=array("crutchy");
+$exec_list=array();
+$data=file_get_contents(EXEC_FILE);
+$data=explode("\n",$data);
+for ($i=0;$i<count($data);$i++)
+{
+  $line=trim($data[$i]);
+  if ($line=="")
+  {
+    continue;
+  }
+  if (substr($line,0,1)=="#")
+  {
+    continue;
+  }
+  $auto="";
+  $alias="";
+  $cmd="";
+  if (parse_exec($line,$auto,$alias,$cmd)==True)
+  {
+    $exec_list[$alias]["auto"]=$auto;
+    $exec_list[$alias]["cmd"]=$cmd;
+  }
+}
 stream_set_blocking(STDIN,False);
 $fp=fsockopen("irc.sylnt.us",6667);
 stream_set_blocking($fp,False);
 fputs($fp,"NICK ".NICK."\n");
-fputs($fp,"USER ".NICK." * ".NICK." :".NICK."\n");
+# USER username hostname servername :realname
+fputs($fp,"USER ".NICK." hostname servername :".NICK."\n");
 $handles=array();
 while (feof($fp)===False)
 {
@@ -27,27 +58,10 @@ while (feof($fp)===False)
   if ($in!==False)
   {
     $tin=trim($in);
-    if (strtoupper($tin)==CMD_QUIT)
+    if (strtolower($tin)==CMD_QUIT)
     {
       doquit($fp);
       return;
-    }
-    else
-    {
-      /*$cmd=substr($tin,0,strpos($tin," "));
-      $msg=substr($tin,strpos($tin," "));
-      if (($cmd<>"") and ($msg<>""))
-      {
-        $n=count($handles);
-        for ($i=0;$i<$n;$i++)
-        {
-          if ($handles[$i]["command"]==$cmd)
-          {
-            fwrite($handles[$i]["pipe_stdin"],$msg);
-          }
-        }
-        privmsg(CHAN_TERM,$tin);
-      }*/
     }
   }
   $n=count($handles);
@@ -58,7 +72,28 @@ while (feof($fp)===False)
       $buf=fgets($handles[$i]["pipe_stdout"]);
       if ($buf!==False)
       {
-        privmsg(CHAN_TERM,rtrim($buf));
+        $pre=substr(strtolower($buf),0,strlen(TERM_PRIVMSG)+1);
+        if (($pre==(TERM_PRIVMSG." ")) or ($handles[$i]["auto_privmsg"]==1))
+        {
+          $msg=rtrim($buf);
+          if ($pre==(TERM_PRIVMSG." "))
+          {
+            $msg=substr($msg,strlen(TERM_PRIVMSG)+1);
+          }
+          privmsg($handles[$i]["chan"],$msg);
+        }
+        else
+        {
+          term_echo(rtrim($buf));
+        }
+      }
+    }
+    while (feof($handles[$i]["pipe_stderr"])==False)
+    {
+      $buf=fgets($handles[$i]["pipe_stderr"]);
+      if ($buf!==False)
+      {
+        term_echo(rtrim($buf));
       }
     }
     $proc_info=proc_get_status($handles[$i]["process"]);
@@ -84,11 +119,56 @@ while (feof($fp)===False)
   {
     append_log($items);
     $params=explode(" ",$items["msg"]);
-    switch (strtoupper($params[0]))
+    switch (strtolower($params[0]))
     {
+      case CMD_ABOUT:
+        privmsg($items["chan"],"IRC SCRIPT EXECUTIVE");
+        privmsg($items["chan"],"  by crutchy: https://github.com/crutchy-/test/blob/master/irc.php");
+        privmsg($items["chan"],"  visit http://wiki.soylentnews.org/wiki/IRC#bacon.2Fcoffee.2Fmother for more info");
+        break;
       case CMD_QUIT:
-        doquit($fp);
-        return;
+        if (in_array($items["nick"],$admin_nicks)==True)
+        {
+          doquit($fp);
+          return;
+        }
+        break;
+      case CMD_ADDEXEC:
+        if (in_array($items["nick"],$admin_nicks)==True)
+        {
+          array_shift($params);
+          $line=implode(" ",$params);
+          $auto="";
+          $alias="";
+          $cmd="";
+          if (parse_exec($line,$auto,$alias,$cmd)==True)
+          {
+            $exec_list[$alias]["auto"]=$auto;
+            $exec_list[$alias]["cmd"]=$cmd;
+            $out="";
+            foreach ($exec_list as $alias => $arr)
+            {
+              if ($out<>"")
+              {
+                $out=$out."\n";
+              }
+              $out=$out.$exec_list[$alias]["auto"].EXEC_DELIM.$alias.EXEC_DELIM.$exec_list[$alias]["cmd"];
+            }
+            if (file_put_contents(EXEC_FILE,trim($out))!==False)
+            {
+              privmsg($items["chan"],"exec file successfully saved");
+            }
+            else
+            {
+              privmsg($items["chan"],"error saving exec file");
+            }
+          }
+          else
+          {
+            privmsg($items["chan"],"invalid exec line \"$line\"");
+          }
+        }
+        break;
       default:
         process_scripts($items);
     }
@@ -132,6 +212,23 @@ function pingpong($fp,$data)
     }
   }
   return False;
+}
+
+function parse_exec($line,&$auto,&$alias,&$cmd)
+{
+  $parts=explode(EXEC_DELIM,$line);
+  if (count($parts)<>3)
+  {
+    return False;
+  }
+  if ((($parts[0]<>"0") and ($parts[0]<>"1")) or ($parts[1]=="") or ($parts[2]==""))
+  {
+    return False;
+  }
+  $auto=$parts[0];
+  $alias=$parts[1];
+  $cmd=$parts[2];
+  return True;
 }
 
 function append_log($items)
@@ -222,12 +319,40 @@ function privmsg($chan,$msg)
 function process_scripts($items)
 {
   global $handles;
+  global $exec_list;
+  $parts=explode(" ",$items["msg"]);
+  $alias=$parts[0];
+  if (isset($exec_list[$alias])==False)
+  {
+    return;
+  }
+  array_shift($parts);
+  $msg=implode(" ",$parts);
+  $msg=filter_msg($msg);
+  $template=$exec_list[$alias]["cmd"];
+  $template=str_replace(TEMPLATE_DELIM.TEMPLATE_MSG.TEMPLATE_DELIM,$msg,$template);
+  $template=str_replace(TEMPLATE_DELIM.TEMPLATE_NICK.TEMPLATE_DELIM,$items["nick"],$template);
+  $template=str_replace(TEMPLATE_DELIM.TEMPLATE_CHAN.TEMPLATE_DELIM,$items["chan"],$template);
+  $command="exec ".$template;
   $cwd=NULL;
   $env=NULL;
   $descriptorspec=array(0=>array("pipe","r"),1=>array("pipe","w"),2=>array("pipe","w"));
-  $process=proc_open("exec ".$items["msg"],$descriptorspec,$pipes,$cwd,$env);
+  $process=proc_open($command,$descriptorspec,$pipes,$cwd,$env);
   stream_set_blocking($pipes[0],0);
-  $handles[]=array("process"=>$process,"command"=>$items["msg"],"pipe_stdin"=>$pipes[0],"pipe_stdout"=>$pipes[1],"pipe_stderr"=>$pipes[2]);
+  $handles[]=array("process"=>$process,"command"=>$command,"pipe_stdin"=>$pipes[0],"pipe_stdout"=>$pipes[1],"pipe_stderr"=>$pipes[2],"alias"=>$alias,"template"=>$exec_list[$alias]["cmd"],"auto_privmsg"=>$exec_list[$alias]["auto"],"nick"=>$items["nick"],"chan"=>$items["chan"]);
+}
+
+function filter_msg($msg)
+{
+  $result="";
+  for ($i=0;$i<strlen($msg);$i++)
+  {
+    if (strpos(VALID_CHARS,$msg[$i])!==False)
+    {
+      $result=$result.$msg[$i];
+    }
+  }
+  return $result;
 }
 
 ?>
---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+# gpl2
+# by crutchy
+# 15-april-2014
+
+# 0/test/php test.php "%%msg%%" "%%chan%%" "%%nick%%"
+
+/*
+array(4) {
+  [0]=>
+  string(10) "script.php"
+  [1]=>
+  string(4) "arg1"
+  [2]=>
+  string(4) "arg2"
+  [3]=>
+  string(4) "arg3"
+}
+*/
+
+echo "this shows on term only\n";
+echo "privmsg msg=".$argv[1]."\n";
+echo "privmsg chan=".$argv[2]."\n";
+echo "privmsg nick=".$argv[3]."\n";
+
+?>
