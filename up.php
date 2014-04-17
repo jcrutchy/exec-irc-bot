bdb9c2988706629a8a098a3a6965fb4ff9aa151c
crutchy <jared.crutchfield@hotmail.com>
2014-04-17 17:38:21 +1000

    updated up.php and disabled lol execs

--- up.php
+++ up.php
@@ -8,6 +8,18 @@
 
 echo "start=".$argv[1]."\n";
 $uptime=microtime(True)-$argv[1];
-echo "privmsg bacon up: ".round($uptime)." seconds\n";
+echo "privmsg bacon up: ".secsToTime($uptime)."\n";
+
+function secsToTime($secs) # by chromas, 17-april-2014
+{
+  $ss=$secs;
+  $dd=floor($secs/86400);
+  $secs=$secs%86400;
+  $hh=floor($secs/3600);
+  $secs=$secs%3600;
+  $mm=floor($secs/60);
+  $secs=$secs%60;
+  return $dd."d ".$hh.":".$mm.":".$secs;
+}
 
 ?>
