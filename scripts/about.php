87dfbec7b5446dd2e6da5832f11d5b8e4f8e47cf
crutchy- <jared.crutchfield@hotmail.com>
2014-04-26 20:36:49 +1000

    update

---
+++ scripts/about.php
@@ -0,0 +1,11 @@
+<?php
+
+# gpl2
+# by crutchy
+# 26-april-2014
+
+echo "IRC SCRIPT EXECUTIVE\n";
+echo "  https://github.com/crutchy-/test\n";
+echo "  http://wiki.soylentnews.org/wiki/IRC:exec\n";
+
+?>
