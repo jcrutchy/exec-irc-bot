c713ba1dcdac1a0d69eb3a5a45199029b81511a2
crutchy- <jared@debian.debian>
2014-05-22 20:46:11 +1000

    reworked define script with urbandictionary. thanks for the suggestion chromas

--- scripts/definitions.php
+++ scripts/definitions.php
@@ -2,37 +2,16 @@
 
 # gpl2
 # by crutchy
-# 23-april-2014
+# 22-may-2014
 
 $msg=$argv[1];
 
-$html=wget("www.wolframalpha.com","/input/?i=define%3A$msg",80);
-
-$delim1="context.jsonArray.popups.pod_0200.push( {\"stringified\": \"";
-$delim2="\",\"mInput\": \"\",\"mOutput\": \"\", \"popLinks\": {} });";
-
-$i=strpos($html,$delim1)+strlen($delim1);
-
-$html=substr($html,$i);
-
-$i=strpos($html,$delim2);
-
-$def=trim(substr($html,0,$i));
-
-if (strlen($def)<700)
+if (urbandictionary($msg)==False)
 {
-  if ($def=="")
+  if (wolframalpha($msg)==False)
   {
     echo "IRC_MSG $msg: unable to find definition\n";
   }
-  else
-  {
-    echo "IRC_MSG $msg: $def\n";
-  }
-}
-else
-{ 
-  echo "$def\n";
 }
 
 function wget($host,$uri,$port)
@@ -51,6 +30,69 @@ function wget($host,$uri,$port)
   }
   fclose($fp);
   return $response;
+}
+
+function wolframalpha($msg)
+{
+  $html=wget("www.wolframalpha.com","/input/?i=define%3A$msg",80);
+  $delim1="context.jsonArray.popups.pod_0200.push( {\"stringified\": \"";
+  $delim2="\",\"mInput\": \"\",\"mOutput\": \"\", \"popLinks\": {} });";
+  $i=strpos($html,$delim1)+strlen($delim1);
+  $html=substr($html,$i);
+  $i=strpos($html,$delim2);
+  $def=trim(substr($html,0,$i));
+  if (strlen($def)<700)
+  {
+    if ($def=="")
+    {
+      return False;
+    }
+    else
+    {
+      echo "IRC_MSG [wolframalpha] $msg: $def\n";
+      return True;
+    }
+  }
+  else
+  { 
+    echo "$def\n";
+    return False;
+  }
+}
+
+function urbandictionary($msg)
+{
+  $html=wget("www.urbandictionary.com","/define.php?term=$msg",80);
+  $delim1="<meta content='";
+  $delim2="' name='Description' property='og:description'>";
+  $i=strpos($html,$delim2);
+  $html=substr($html,0,$i);
+  $def="";
+  for ($j=$i;$j>0;$j--)
+  {
+    if (substr($html,$j,strlen($delim1))==$delim1)
+    {
+      $def=trim(substr($html,$j+strlen($delim1)));
+      break;
+    }
+  }
+  if (strlen($def)<700)
+  {
+    if ($def=="")
+    {
+      return False;
+    }
+    else
+    {
+      echo "IRC_MSG [urbandictionary] $msg: $def\n";
+      return True;
+    }
+  }
+  else
+  { 
+    echo "$def\n";
+    return False;
+  }
 }
 
 ?>
