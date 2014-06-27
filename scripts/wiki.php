41064efd842b62de564cc01ed99cf2b6e254b233
crutchy- <jared@debian.debian>
2014-06-27 23:27:54 +1000

    got basic wiki editing working

--- scripts/wiki.php
+++ scripts/wiki.php
@@ -19,20 +19,17 @@ $alias=$argv[4];
 define("WIKI_USER_AGENT","IRC-Executive/0.01 (https://github.com/crutchy-/test/blob/master/scripts/wiki.php; jared.crutchfield@hotmail.com)");
 define("WIKI_HOST","wiki.soylentnews.org");
 
-$cookie=get_bucket("wiki_login_cookieprefix");
+$login=get_bucket("wiki_login_cookieprefix");
 
-if ($cookie=="")
+if ($login=="")
 {
   switch (strtolower($trailing))
   {
     case "login":
       login();
       break;
-    case "logout":
-      logout();
-      break;
     default:
-      privmsg("not logged in");
+      privmsg("wiki: not logged in");
   }
 }
 else
@@ -42,10 +39,24 @@ else
   switch (strtolower($action))
   {
     case "edit":
-      edit();
+      array_shift($parts);
+      $trailing=implode(" ",$parts);
+      $parts=explode("|",$trailing);
+      if (count($parts)<>3)
+      {
+        privmsg("syntax: ~wiki title|section|text");
+        return;
+      }
+      $title=$parts[0];
+      $section=$parts[1];
+      $text=$parts[2];
+      edit($title,$section,$text);
+      break;
+    case "logout":
+      logout();
       break;
     default:
-      privmsg($cookie);
+      privmsg("wiki: no action specified");
   }
 }
 
@@ -74,11 +85,11 @@ function logout()
   unset_bucket("wiki_login_sessionid");
   if ($loggedout==True)
   {
-    privmsg("successfully logged out of wiki");
+    privmsg("wiki: successfully logged out");
   }
   else
   {
-    privmsg("logout confirmation not received");
+    privmsg("wiki: logout confirmation not received");
   }
   return;
 }
@@ -96,7 +107,7 @@ function login()
   $params["lgtoken"]=$data["login"]["token"];
   $response=wpost(WIKI_HOST,"/w/api.php?action=login&format=php",80,WIKI_USER_AGENT,$params,$headers);
   $data=unserialize(strip_headers($response));
-  $msg="wiki login=".$data["login"]["result"];
+  $msg="wiki: login=".$data["login"]["result"];
   if ($data["login"]["result"]=="Success")
   {
     $msg=$msg.", username=".$data["login"]["lgusername"]." (userid=".$data["login"]["lguserid"].")";
@@ -108,58 +119,92 @@ function login()
 
 #####################################################################################################
 
-function edit()
+function edit($title,$section,$text)
 {
-
+  if (($title=="") or ($section==""))
+  {
+    privmsg("wiki: edit=invalid title/section");
+    return;
+  }
   $cookieprefix=get_bucket("wiki_login_cookieprefix");
   $sessionid=get_bucket("wiki_login_sessionid");
   if (($cookieprefix=="") or ($sessionid==""))
   {
-    privmsg("edit: not logged in");
+    privmsg("wiki: edit=not logged in");
     return;
   }
   $headers=array("Cookie"=>login_cookie($cookieprefix,$sessionid));
-
   $uri="/w/api.php?action=tokens&format=php";
-
   $response=wget(WIKI_HOST,$uri,80,WIKI_USER_AGENT,$headers);
-
   $data=unserialize(strip_headers($response));
-
   if (isset($data["tokens"]["edittoken"])==False)
   {
-    privmsg("edit: error getting edittoken");
+    privmsg("wiki: edit=error getting edittoken");
     return;
   }
-
   $token=$data["tokens"]["edittoken"];
-
+  $uri="/w/api.php?action=parse&format=php&page=".urlencode($title)."&prop=sections";
+  $response=wget(WIKI_HOST,$uri,80,WIKI_USER_AGENT,$headers);
+  $data=unserialize(strip_headers($response));
+  if (isset($data["parse"]["sections"])==False)
+  {
+    privmsg("wiki: edit=error getting sections for page \"".$title."\"");
+    return;
+  }
+  var_dump($data);
+  $sections=$data["parse"]["sections"];
+  $index=-1;
+  for ($i=0;$i<count($sections);$i++)
+  {
+    $line=$sections[$i]["line"];
+    if ($section==$line)
+    {
+      $index=$i;
+      break;
+    }
+  }
+  if ($index<0)
+  {
+    $index="new";
+  }
+  else
+  {
+    if (isset($sections[$index]["index"])==False)
+    {
+      privmsg("wiki: edit=section not found");
+      return;
+    }
+    $index=$sections[$index]["index"];
+  }
   $uri="/w/api.php?action=edit";
-
   # http://www.mediawiki.org/wiki/API:Edit#Parameters
   $params=array(
     "format"=>"php",
-    "title"=>"Sandbox",
-    #"section"=>"new",
-    #"sectiontitle"=>"exec test",
-    "section"=>"exec test",
-    "summary"=>"exec test",
-    "text"=>"trying something else",
-    #"appendtext"=>"Hello world!",
+    "title"=>$title,
+    "section"=>$index,
+    "summary"=>$section,
+    "text"=>$text,
+    "contentformat"=>"text/x-wiki",
+    "contentmodel"=>"wikitext",
     "bot"=>"",
     "token"=>$token);
-
+  var_dump($params);
   $response=wpost(WIKI_HOST,$uri,80,WIKI_USER_AGENT,$params,$headers);
-
   $data=unserialize(strip_headers($response));
-
-  $msg="wiki edit=".$data["edit"]["result"];
-  if ($data["edit"]["result"]=="Success")
-  {
-    $msg=$msg.", oldrevid=".$data["edit"]["oldrevid"].", newrevid=".$data["edit"]["newrevid"];
-  }
-
   var_dump($data);
+  if (isset($data["error"])==True)
+  {
+    $msg="wiki edit=".$data["error"]["code"];
+  }
+  else
+  {
+    $msg="wiki edit=".$data["edit"]["result"];
+    if ($data["edit"]["result"]=="Success")
+    {
+      $msg=$msg.", oldrevid=".$data["edit"]["oldrevid"].", newrevid=".$data["edit"]["newrevid"];
+    }
+  }
+  privmsg($msg);
 }
 
 #####################################################################################################
