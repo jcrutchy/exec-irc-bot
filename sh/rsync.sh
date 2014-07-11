9c885174d2e9d22f5f58cc1e8fcb7245ca6f21e2
crutchy- <jared@debian.debian>
2014-07-11 22:46:11 +1000

    update

--- sh/rsync.sh
+++ sh/rsync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-rsync -av /nas/server/git/test/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc
+rsync -avzru /nas/server/git/test/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc/
+rsync -avzru jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc/ /nas/server/git/test/
 exit 0
