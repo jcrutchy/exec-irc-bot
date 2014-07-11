b75293cb5ea86a2fe927c42f14a8fb313e018696
Jared <jared@squishy.(none)>
2014-07-11 23:09:03 +1000

    update

--- sh/rsync.sh
+++ sh/rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -avzru /nas/server/git/test/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc/
-rsync -avzru jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc/ /nas/server/git/test/
+git fetch
+rsync -av /nas/server/git/test/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc/
 exit 0
