5925b669b4df1dcd1fc24d21f1a5bd10491f0b34
crutchy- <jared@debian.debian>
2014-07-11 23:55:46 +1000

    updated shell scripts

--- sh/publish.sh
+++ sh/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 bash /nas/server/git/test/sh/sync.sh
-if [ $1 -eq "" ]; then
+if [ $1 == "" ]; then
  bash /nas/server/git/test/sh/push.sh "update"
 else
  bash /nas/server/git/test/sh/push.sh "$1"
