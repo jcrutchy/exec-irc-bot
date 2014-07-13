0be2ff39b7e75adb2bd8c9b9e56e5d2bb1a55238
crutchy- <jared@debian.debian>
2014-07-14 01:02:27 +1000

    update

--- sh/publish.sh
+++ sh/publish.sh
@@ -2,9 +2,9 @@
 
 bash /nas/server/git/test/sh/sync.sh
 if [ "$1" = "" ]; then
- bash /nas/server/git/test/sh/push.sh "update"
+  bash /nas/server/git/test/sh/push.sh "update"
 else
- bash /nas/server/git/test/sh/push.sh "$1"
+  bash /nas/server/git/test/sh/push.sh "$1"
 fi
 
 exit 0
--- sh/push.sh
+++ sh/push.sh
@@ -2,9 +2,9 @@
 
 git add *
 if [ "$1" = "" ]; then
- git commit -a -m "update"
+  git commit -a -m "update"
 else
- git commit -a -m "$1"
+  git commit -a -m "$1"
 fi
 git push
 exit 0
