4c45e05a318baced08c42d108b31e74b1afa71c6
crutchy- <jared@debian.debian>
2014-07-19 18:58:16 +1000

    update

--- scripts/log.php
+++ scripts/log.php
@@ -10,73 +10,66 @@ define("IRC_INDEX_HTML_HEAD","<!DOCTYPE HTML>\n<html>\n<head>\n<title>SoylentNew
 define("IRC_CHAN_INDEX_HEAD","<!DOCTYPE HTML>\n<html>\n<head>\n<title>%%title%%</title>\n<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">\n".HTML_NO_CACHE."<style type=\"text/css\"></style>\n<script type=\"text/javascript\"></script>\n</head>\n<body>\n<p><a href=\"index.html\">return to channel index</a></p>\n<p>\n");
 define("IRC_LOG_HEAD","<!DOCTYPE HTML>\n<html>\n<head>\n<title>%%title%%</title>\n<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\">\n".HTML_NO_CACHE."<style type=\"text/css\"></style>\n<script type=\"text/javascript\"></script>\n</head>\n<body>\n<p><a href=\"%%index_href%%\">return to date index</a></p>\n<p>\n");
 
-function log_items($items)
+$trailing=$argv[1];
+$dest=$argv[1];
+$nick=$argv[1];
+$start=$argv[1];
+$alias=$argv[1];
+$cmd=$argv[1];
+$data=$argv[1];
+$exec=$argv[1];
+$params=$argv[1];
+
+if (($dest=="") or (substr($dest,0,1)<>"#") or (strpos($dest," ")!==False))
 {
-  global $log_chans;
-  $dest=$items["destination"];
-  $cmd=$items["cmd"];
-  $trailing=$items["trailing"];
-  $nick=$items["nick"];
-  if (($dest=="") or (substr($dest,0,1)<>"#") or (strpos($dest," ")!==False))
-  {
-    return;
-  }
-  if (isset($log_chans[$dest])==True)
-  {
-    if ($log_chans[$dest]=="off")
-    {
-      return;
-    }
-  }
-  else
-  {
-    return;
-  }
-  if (file_exists(IRC_LOG_INDEX_FILE)==False)
-  {
-    file_put_contents(IRC_LOG_INDEX_FILE,IRC_INDEX_SOURCE);
-  }
-  if (file_exists(IRC_LOG_INDEX_FILE_HTML)==False)
-  {
-    file_put_contents(IRC_LOG_INDEX_FILE_HTML,IRC_INDEX_HTML_HEAD);
-  }
-  $contents=file_get_contents(IRC_LOG_INDEX_FILE_HTML);
-  $chan_enc=urlencode($dest);
-  if (strpos($contents,$dest)===False)
-  {
-    $line="<a href=\"index_$chan_enc.html\">$dest</a><br>\n";
-    file_put_contents(IRC_LOG_INDEX_FILE_HTML,$line,FILE_APPEND);
-  }
-  $timestamp=date("H:i:s",microtime(True));
-  $timestamp_name=date("His",microtime(True));
-  $filename=IRC_LOG_PATH.$dest."_".date("Ymd",time()).".html";
-  $filename_href=urlencode($dest)."_".date("Ymd",time()).".html";
-  $href_caption=date("Y-m-d",time());
-
-  $data="&lt;$nick&gt; $trailing";
-
-  $line="<a href=\"#$timestamp_name\" name=\"$timestamp_name\" class=\"time\">[$timestamp]</a> $data<br>\n";
-  if (file_exists($filename)==False)
-  {
-    $chan_index_filename=IRC_LOG_PATH."index_".$dest.".html";
-    if (file_exists($chan_index_filename)==False)
-    {
-      $head=IRC_CHAN_INDEX_HEAD;
-      $head=str_replace("%%title%%","$dest | SoylentNews IRC Log",$head);
-      file_put_contents($chan_index_filename,$head);
-    }
-    $contents=file_get_contents($chan_index_filename);
-    if (strpos($contents,$filename_href)===False)
-    {
-      $line_chan_index="<a href=\"$filename_href\">$href_caption</a><br>\n";
-      file_put_contents($chan_index_filename,$line_chan_index,FILE_APPEND);
-    }
-    $head=IRC_LOG_HEAD;
-    $head=str_replace("%%title%%","$dest | $href_caption",$head);
-    $head=str_replace("%%index_href%%","index_$chan_enc.html",$head);
-    file_put_contents($filename,$head);
-  }
-  file_put_contents($filename,$line,FILE_APPEND);
+  return;
 }
+if (file_exists(IRC_LOG_INDEX_FILE)==False)
+{
+  file_put_contents(IRC_LOG_INDEX_FILE,IRC_INDEX_SOURCE);
+}
+if (file_exists(IRC_LOG_INDEX_FILE_HTML)==False)
+{
+  file_put_contents(IRC_LOG_INDEX_FILE_HTML,IRC_INDEX_HTML_HEAD);
+}
+$contents=file_get_contents(IRC_LOG_INDEX_FILE_HTML);
+$chan_enc=urlencode($dest);
+if (strpos($contents,$dest)===False)
+{
+  $line="<a href=\"index_$chan_enc.html\">$dest</a><br>\n";
+  file_put_contents(IRC_LOG_INDEX_FILE_HTML,$line,FILE_APPEND);
+}
+$t=microtime(True);
+$ts=time();
+$timestamp=date("H:i:s",$t);
+$timestamp_name=date("His",microtime(True));
+$filename=IRC_LOG_PATH.$dest."_".date("Ymd",$ts).".html";
+$filename_href=urlencode($dest)."_".date("Ymd",$ts).".html";
+$href_caption=date("Y-m-d",$ts);
+
+$data="&lt;$nick&gt; $trailing";
+
+$line="<a href=\"#$timestamp_name\" name=\"$timestamp_name\" class=\"time\">[$timestamp]</a> $data<br>\n";
+if (file_exists($filename)==False)
+{
+  $chan_index_filename=IRC_LOG_PATH."index_".$dest.".html";
+  if (file_exists($chan_index_filename)==False)
+  {
+    $head=IRC_CHAN_INDEX_HEAD;
+    $head=str_replace("%%title%%","$dest | SoylentNews IRC Log",$head);
+    file_put_contents($chan_index_filename,$head);
+  }
+  $contents=file_get_contents($chan_index_filename);
+  if (strpos($contents,$filename_href)===False)
+  {
+    $line_chan_index="<a href=\"$filename_href\">$href_caption</a><br>\n";
+    file_put_contents($chan_index_filename,$line_chan_index,FILE_APPEND);
+  }
+  $head=IRC_LOG_HEAD;
+  $head=str_replace("%%title%%","$dest | $href_caption",$head);
+  $head=str_replace("%%index_href%%","index_$chan_enc.html",$head);
+  file_put_contents($filename,$head);
+}
+file_put_contents($filename,$line,FILE_APPEND);
 
 ?>
