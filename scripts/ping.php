a3a8adc9a18af459a735260445f309acc6831c19
crutchy- <jared@debian.debian>
2014-07-19 14:08:20 +1000

    update

--- scripts/ping.php
+++ scripts/ping.php
@@ -10,36 +10,29 @@ ini_set("display_errors","on");
 date_default_timezone_set("UTC");
 require_once("lib.php");
 $trailing=trim($argv[1]);
-define("BUCKET_PING_LAG","<<PING_LAG>>");
-$ping_lag=get_bucket(BUCKET_PING_LAG);
+define("BUCKET_PING","<<PING>>");
+define("BUCKET_PONG","<<PONG>>");
+$ping=get_bucket(BUCKET_PING);
+$pong=get_bucket(BUCKET_PONG);
 $t=microtime(True);
 if ($trailing<>"")
 {
-  if ($ping_lag=="")
-  {
-    return;
-  }
-  $delta=$t-$ping_lag;
-  #pm("#","lag = $delta sec");
-  if ($delta>10)
-  {
-    term_echo("==================== PING TIMEOUT DETECTED ====================");
-    echo "/INTERNAL ~restart\n";
-  }
+  set_bucket(BUCKET_PONG,$trailing);
+  pm("#","PONG $trailing");
 }
 else
 {
-  if ($ping_lag<>"")
+  if ($ping<>"")
   {
-    if ($ping_lag==$t)
+    if ($ping<>$pong)
     {
       term_echo("==================== PING TIMEOUT DETECTED ====================");
       echo "/INTERNAL ~restart\n";
     }
   }
-  set_bucket(BUCKET_PING_LAG,$t);
+  set_bucket(BUCKET_PING,$t);
   $msg="PING $t";
-  #pm("#",$msg);
+  pm("#",$msg);
   rawmsg($msg);
 }
 
