dba2e49f808693586746be95d23920f6d379b98b
crutchy- <jared@debian.debian>
2014-07-24 21:51:42 +1000

    update

--- scripts/about.php
+++ scripts/about.php
@@ -10,7 +10,7 @@ if ($argv[1]<>"")
 }
 
 echo "IRC SCRIPT EXECUTIVE\n";
-echo "  https://github.com/crutchy-/test\n";
 echo "  http://wiki.soylentnews.org/wiki/IRC:exec\n";
+echo "  https://github.com/crutchy-/test\n";
 
 ?>
