b7114b46d6d62271689d93e9f7e43e36afafaa91
crutchy- <jared@debian.debian>
2014-08-16 17:09:59 +1000

    update

--- scripts/about.php
+++ scripts/about.php
@@ -2,7 +2,7 @@
 
 # gpl2
 # by crutchy
-# 26-april-2014
+# 16-aug-2014
 
 if ($argv[1]<>"")
 {
@@ -11,6 +11,6 @@ if ($argv[1]<>"")
 
 echo "IRC SCRIPT EXECUTIVE\n";
 echo "  http://wiki.soylentnews.org/wiki/IRC:exec\n";
-echo "  https://github.com/crutchy-/test\n";
+echo "  https://github.com/crutchy-/exec-irc-bot\n";
 
 ?>
