0ded678f81fdcc8f8fdb4bd5193563bdbce33da3
crutchy- <jared@debian.debian>
2014-08-31 08:48:03 +1000

    update

--- scripts/admin.php
+++ scripts/admin.php
@@ -2,15 +2,39 @@
 
 # gpl2
 # by crutchy
-# 12-july-2014
+# 30-aug-2014
 
 #####################################################################################################
 
 ini_set("display_errors","on");
 require_once("lib.php");
 
-$dest=$argv[1];
-echo "/IRC :".NICK_EXEC." MODE $dest +o crutchy\n";
+$trailing=strtolower(trim($argv[1]));
+$dest=strtolower(trim($argv[2]));
+$nick=strtolower(trim($argv[3]));
+$alias=strtolower(trim($argv[4]));
+
+$target=$nick;
+if ($trailing<>"")
+{
+  $target=$trailing;
+}
+
+switch ($alias)
+{
+  case "~op":
+    rawmsg("MODE $dest +o $target");
+    break;
+  case "~deop":
+    rawmsg("MODE $dest -o $target");
+    break;
+  case "~voice":
+    rawmsg("MODE $dest +v $target");
+    break;
+  case "~devoice":
+    rawmsg("MODE $dest -v $target");
+    break;
+}
 
 #####################################################################################################
 
