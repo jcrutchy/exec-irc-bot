716b03b7f8e1d80648746fc305dbaad4836ebd20
crutchy- <jared@debian.debian>
2014-09-15 08:06:24 +1000

    update

--- scripts/weather.php
+++ scripts/weather.php
@@ -6,21 +6,6 @@
 
 #####################################################################################################
 
-# requested by kobach: weather request, Spirit Of Saint Louis, Missouri (38.7°N/90.7°W), Updated: 1:54 PM CST (December 23, 2013), Conditions: Mostly Cloudy, Temperature: 26°F (-3.3°C), Windchill: 16°F (-9°C), High/Low: 26/9°F (-3.3/-12.8°C), UV: 1/16, Humidity: 66%, Dew Point: 16°F (-8.9°C), Pressure: 30.51 in/1033 hPa, Wind: WNW at 10 MPH (17 KPH)
-
-# http://wxqa.com/APRSWXNETStation.txt
-# EW4841|E4841|EW4841 Murrumbena                    AU|45|  -37.90783|145.07217|GMT|||1||||
-# http://www.wxqa.com/cgi-bin/search1.cgi?keyword=EW4841
-# http://www.findu.com/cgi-bin/wx.cgi?call=EW4841&units=metric
-
-# http://www.wunderground.com/weather/api/
-# http://openweathermap.org/city/756135
-
-# TODO: registered nick personalised settings (units, default location, private msg, formatting, etc)
-# TODO: change to use google (like time script)
-
-#####################################################################################################
-
 require_once("lib.php");
 require_once("weather_lib.php");
 require_once("time_lib.php");
@@ -31,6 +16,9 @@ $nick=trim($argv[3]);
 
 switch ($alias)
 {
+  case "~weather-prefs":
+    # TODO: registered nick personalised settings (units, default location, private msg, formatting, etc)
+    break;
   case "~weather-del":
     if (del_location($trailing)==True)
     {
@@ -61,7 +49,10 @@ switch ($alias)
     {
       privmsg("weather error: $trailing");
     }
-    /*if (is_array($data)==False)
+    break;
+  case "~weather-old":
+    $data=process_weather_old($trailing,$nick);
+    if (is_array($data)==False)
     {
       switch ($data)
       {
@@ -88,7 +79,7 @@ switch ($alias)
       }
       $color="10";
       privmsg("weather for ".chr(2).chr(3).$color.$data["name"].chr(3).chr(2)." at $time_str".$data["age"]." temp: ".chr(2).chr(3).$color.$data["temp"].chr(3).chr(2).", dp: ".chr(2).chr(3).$color.$data["dewpoint"].chr(3).chr(2).", press: ".chr(2).chr(3).$color.$data["press"].chr(3).chr(2).", humid: ".chr(2).chr(3).$color.$data["humidity"].chr(3).chr(2).", wind: ".chr(2).chr(3).$color.$data["wind_speed"].chr(3).chr(2)." @ ".chr(2).chr(3).$color.$data["wind_direction"].chr(3).chr(2));
-    }*/
+    }
     break;
 }
 
