479849f8f2ce776f3efede0ab3bfd51c87d7fe5f
crutchy- <jared@debian.debian>
2014-10-10 23:18:47 +1100

    update

--- sh/publish.sh
+++ sh/publish.sh
@@ -2,13 +2,8 @@
 
 # copy to bottom of /.git/config
 # [alias]
-#  up = !bash sh/publish.sh $1
+#  up = !bash sh/publish.sh
 
 bash /nas/server/git/exec-irc-bot/sh/sync.sh
-if [ "$1" = "" ]; then
-  bash /nas/server/git/exec-irc-bot/sh/push.sh "update"
-else
-  bash /nas/server/git/exec-irc-bot/sh/push.sh "$1"
-fi
-
+bash /nas/server/git/exec-irc-bot/sh/push.sh
 exit 0
--- sh/push.sh
+++ sh/push.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 git add *
-if [ "$1" = "" ]; then
-  git commit -a -m "update"
-else
-  git commit -a -m "$1"
-fi
+git commit -a -m "update"
 git push
 exit 0
