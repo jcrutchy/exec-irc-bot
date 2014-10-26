bbb481ee387ffd5cdc3abc856fffee9ec592435e
crutchy- <jared@debian.debian>
2014-10-26 22:28:51 +1100

    whatever i changed

--- scripts/vote.php
+++ scripts/vote.php
@@ -3,28 +3,6 @@
 # gpl2
 # by crutchy
 
-/*
-
-# http://wiki.soylentnews.org/wiki/IRC:exec#Proposed_IRC_voting_system
-
-    user connects to Soylent IRC and identifies with NickServ
-    user can get list of available polls using ~vote list (system will allow multiple concurrent polls)
-    optional time limit or due time for voting
-    user can get list of available preferences for a given vote id using ~vote list <vote_id>
-    user can get help on voting using ~vote, ~vote help, ~vote-help or ~vote ?
-    user registers to vote using ~vote register <vote_id> <email_address>
-    email address must be unique per vote
-    bot emails a vote key, which is only good for that user and that vote (key will be a short unique string of random characters, eg: Ar7u2y6T5koBW)
-    user votes using /msg exec ~vote <vote_id> <key> <preference>
-    if flag set by vote admin, users can suggest vote preference using /msg exec ~vote suggest <vote_id> <key> <preference>
-    creating polls by authorized staff/admins to be done via IRC commands
-    administrator can optionally set a flag to enable or disable multiple use of same key, and whether new votes replace old votes or cumulate
-    adminstrator can see results with ~vote results <vote_id>, or publish to channel with ~vote publish <vote_id>
-    bot uses secure connection to IRC, but emailing of keys will be in plain text
-    if necessary, possibly ban use of some web email hosts such as hotmail, yahoo, gmail, etc
-
-*/
-
 #####################################################################################################
 
 require_once("lib.php");
@@ -32,6 +10,12 @@ require_once("lib.php");
 $trailing=strtolower(trim($argv[1]));
 $dest=strtolower(trim($argv[2]));
 $nick=strtolower(trim($argv[3]));
+
+if (($trailing=="") or ($trailing=="?") or ($trailing=="help"))
+{
+  privmsg("  http://sylnt.us/vote");
+  return;
+}
 
 $parts=explode(" ",$trailing);
 delete_empty_elements($parts);
@@ -48,7 +32,7 @@ switch ($id)
   case "list":
     if (count($data)==0)
     {
-      privmsg("  no votes registered");
+      privmsg("  no polls registered");
     }
     else
     {
@@ -69,11 +53,11 @@ if (isset($parts[0])==True)
   {
     if ($id=="")
     {
-      privmsg("  you must specify a vote id");
+      privmsg("  you must specify a poll id");
     }
     if (($id=="register") or ($id=="list"))
     {
-      privmsg("  invalid vote id");
+      privmsg("  invalid poll id");
     }
     else
     {
@@ -88,7 +72,7 @@ if (isset($parts[0])==True)
       $data[$id]["accounts"]=array();
       $data[$id]["admins"]=array($account);
       set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-      privmsg("  vote \"$id\" registered");
+      privmsg("  poll \"$id\" registered");
     }
   }
   elseif (isset($data[$id])==True)
@@ -99,12 +83,12 @@ if (isset($parts[0])==True)
         $account=users_get_account($nick);
         if ($data[$id]["founder"]<>$account)
         {
-          privmsg("  only the vote founder can unregister the vote");
+          privmsg("  only the poll founder can unregister the poll");
           return;
         }
         unset($data[$id]);
         set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-        privmsg("  vote \"$id\" unregistered");
+        privmsg("  poll \"$id\" unregistered");
         return;
       case "result":
         $result=0;
@@ -112,13 +96,13 @@ if (isset($parts[0])==True)
         {
           $result=$result+$value;
         }
-        privmsg("  tally of votes for \"$id\" => $result");
+        privmsg("  tally of votes for poll \"$id\" => $result");
         return;
       case "breakdown":
         $account=users_get_account($nick);
         if (in_array($account,$data[$id]["admins"])==False)
         {
-          privmsg("  only a vote admin may output vote breakdown");
+          privmsg("  only a poll admin may output the vote breakdown for a poll");
           return;
         }
         foreach ($data[$id]["accounts"] as $account => $value)
@@ -130,7 +114,7 @@ if (isset($parts[0])==True)
         $account=users_get_account($nick);
         if ($data[$id]["founder"]<>$account)
         {
-          privmsg("  only the vote founder can add vote admins");
+          privmsg("  only the poll founder can add poll admins");
           return;
         }
         $admin_account=users_get_account($parts[0]);
@@ -142,18 +126,18 @@ if (isset($parts[0])==True)
         {
           $data[$id]["admins"][]=$admin_account;
           set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-          privmsg("  account \"$admin_account\" added as admin for vote \"$id\"");
+          privmsg("  account \"$admin_account\" added as admin for poll \"$id\"");
         }
         else
         {
-          privmsg("  admin \"$admin_account\" already exists for vote \"$id\"");
+          privmsg("  admin \"$admin_account\" already exists for poll \"$id\"");
         }
         return;
       case "del-admin":
         $account=users_get_account($nick);
         if ($data[$id]["founder"]<>$account)
         {
-          privmsg("  only the vote founder can delete vote admins");
+          privmsg("  only the poll founder can delete poll admins");
           return;
         }
         $admin_account=$parts[0];
@@ -163,58 +147,58 @@ if (isset($parts[0])==True)
           unset($data[$id]["admins"][$index]);
           $data[$id]["admins"]=array_values($data[$id]["admins"]);
           set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-          privmsg("  account \"$admin_account\" deleted froms admins for vote \"$id\"");
+          privmsg("  account \"$admin_account\" deleted froms admins for poll \"$id\"");
         }
         else
         {
-          privmsg("  admin \"$admin_account\" not found in admins for vote \"$id\"");
+          privmsg("  admin \"$admin_account\" not found in admins for poll \"$id\"");
         }
         return;
       case "open":
         $account=users_get_account($nick);
         if (in_array($account,$data[$id]["admins"])==False)
         {
-          privmsg("  only a vote admin may open the vote");
+          privmsg("  only a poll admin may open the poll for voting");
           return;
         }
         $data[$id]["status"]="open";
         set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-        privmsg("  vote \"$id\" opened");
+        privmsg("  poll \"$id\" opened for voting");
         return;
       case "close":
         $account=users_get_account($nick);
         if (in_array($account,$data[$id]["admins"])==False)
         {
-          privmsg("  only a vote admin may close the vote");
+          privmsg("  only a poll admin may close the poll for voting");
           return;
         }
         $data[$id]["status"]="closed";
         set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-        privmsg("  vote \"$id\" closed");
+        privmsg("  poll \"$id\" closed for voting");
         return;
       case "+":
       case "up":
         if ($data[$id]["status"]<>"open")
         {
-          privmsg("  vote \"$id\" not currently open for voting");
+          privmsg("  poll \"$id\" is not currently open for voting");
           return;
         }
         $account=users_get_account($nick);
         $data[$id]["accounts"][$account]=1;
         set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-        privmsg("  upvote registered for account \"$account\"");
+        privmsg("  upvote registered for poll \"$id\" by account \"$account\"");
         return;
       case "-":
       case "down":
         if ($data[$id]["status"]<>"open")
         {
-          privmsg("  vote \"$id\" not currently open for voting");
+          privmsg("  poll \"$id\" is not currently open for voting");
           return;
         }
         $account=users_get_account($nick);
         $data[$id]["accounts"][$account]=-1;
         set_array_bucket($data,"<<IRC_VOTE_DATA>>");
-        privmsg("  downvote registered for account \"$account\"");
+        privmsg("  downvote registered for poll \"$id\" by account \"$account\"");
         return;
     }
   }
