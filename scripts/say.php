56501240e382904c54976726d4a57a037fed37e4
crutchy- <jared@debian.debian>
2014-11-01 19:17:01 +1100

    whatever i changed

--- scripts/say.php
+++ scripts/say.php
@@ -10,10 +10,16 @@ require_once("lib.php");
 
 $trailing=$argv[1];
 $parts=explode(" ",$trailing);
-$dest=strtolower($parts[0]);
-$ischan=users_chan_exists($dest);
-$isnick=users_nick_exists($dest);
-
+$dest=substr(strtolower($parts[0]),1,strlen($parts[0])-2);
+$ischan=False;
+$isnick=False;
+$delim1=substr($parts[0],0,1);
+$delim2=substr($parts[0],strlen($parts[0])-1);
+if (($delim1=="<") and ($delim2==">"))
+{
+  $ischan=users_chan_exists($dest);
+  $isnick=users_nick_exists($dest);
+}
 if (($ischan==True) or ($isnick==True))
 {
   array_shift($parts);
