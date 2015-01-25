7a2cbe578f61af77ab1b8224e896b02a710a7322
crutchy- <jared@debian.debian>
2015-01-25 16:37:23 +1100

    whatever i changed

--- scripts/lib_http.php
+++ scripts/lib_http.php
@@ -23,7 +23,7 @@ function shorten_url($url)
   }
   $params=array();
   $params["url"]=$url;
-  $response=wpost("z.cr.rs","/","80",ICEWEASEL_UA,$params,"",30);
+  $response=wpost("o.my.to","/","80",ICEWEASEL_UA,$params,"",30);
   $short_url=trim(strip_headers($response));
   if ($short_url<>"")
   {
