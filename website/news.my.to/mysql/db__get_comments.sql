671dbee895bc91ff26dea81e91b10f770a51be84
crutchy <jared.crutchfield@edr.net.au>
2015-03-28 10:24:07 +1100

    whatever i changed

--- website/news.my.to/mysql/db__get_comments.sql
+++
@@ -1,3 +0,0 @@
-SELECT * FROM news_my_to.comments AS p1
-LEFT JOIN (SELECT `cid` AS cid_score,SUM(`mod`) AS score FROM news_my_to.comment_mods GROUP BY `cid`) AS p2 ON p1.`cid`=p2.cid_score
-WHERE `sid`=:sid
