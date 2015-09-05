9b81772713dd7e54e5abe6b1af7794b307efdb94
crutchy <jared@exec-dev>
2015-09-05 20:49:27 +1000

    whatever i changed

--- scripts/python/yukiku.py
+++ scripts/python/yukiku.py
@@ -1,20 +1,24 @@
 """
 exec:~yukiku|10|0|1|1|*||||python scripts/python/yukiku.py %%trailing%% 2>&1
+script by Alex
+https://github.com/Dhs92
 """
 
 import sys
- 
+
+arg = sys.argv
+
 def function_name(arg):
-    if arg == 0 and arg.isdigit:
+    if arg == 0:
         return True
     else:
-        return False
- 
+        return arg
+
 def function_call(arg):
-    arg1 = function_name(sys.argv)
-    if arg1 == True:
+    arg1 = function_name(arg)
+    if arg1:
         print "I'm true!"
     else:
         print arg1
- 
-function_call(sys.argv)
+
+function_call(arg)
