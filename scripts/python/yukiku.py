a3220fe147002678363f4a9e58364cc41c012831
crutchy <jared@exec-dev>
2015-09-05 20:36:34 +1000

    whatever i changed

---
+++ scripts/python/yukiku.py
@@ -0,0 +1,20 @@
+"""
+exec:~yukiku|10|0|1|1|*||||python scripts/python/yukiku.py %%trailing%%
+"""
+
+import sys
+ 
+def function_name(arg):
+    if arg == 0 and arg.isdigit:
+        return True
+    else:
+        return False
+ 
+def function_call(arg):
+    arg1 = function_name(sys.argv)
+    if arg1 == True:
+        print "I'm true!"
+    else:
+        print arg1
+ 
+function_call(sys.arg)
