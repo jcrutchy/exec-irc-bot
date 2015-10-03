468814579b32433fb5f45e2fa7a6e09d8f7c9478
crutchy <jared@exec-dev>
2015-10-03 20:49:13 +1000

    whatever i changed

--- sh/serv.sh
+++ sh/serv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -av /home/jared/git/exec-irc-bot/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc/
+rsync -av /home/jared/git/exec-irc-bot/ jared@192.168.0.21:/var/include/vhosts/irciv.bot.nu/inc/
 exit 0
--- sh/sync.sh
+++ sh/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rsync -av /home/jared/git/exec-irc-bot/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/inc/
-rsync -av /home/jared/git/data/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/data/
-rsync -av /home/jared/git/pwd/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/pwd/
-#rsync -av /home/jared/git/relay/ jared@192.168.0.21:/var/include/vhosts/irciv.us.to/relay/
-rsync -av /var/www/irciv.us.to/ jared@192.168.0.21:/var/www/irciv.us.to/
+rsync -av /home/jared/git/exec-irc-bot/ jared@192.168.0.21:/var/include/vhosts/irciv.bot.nu/inc/
+rsync -av /home/jared/git/data/ jared@192.168.0.21:/var/include/vhosts/irciv.bot.nu/data/
+rsync -av /home/jared/git/pwd/ jared@192.168.0.21:/var/include/vhosts/irciv.bot.nu/pwd/
+#rsync -av /home/jared/git/relay/ jared@192.168.0.21:/var/include/vhosts/irciv.bot.nu/relay/
+rsync -av /var/www/irciv.bot.nu/ jared@192.168.0.21:/var/www/irciv.bot.nu/
 exit 0
