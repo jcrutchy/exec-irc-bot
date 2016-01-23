0b417b7c5753fa59cc35f702328d267570060c44
crutchy <jared@debian>
2016-01-23 20:01:47 +1100

    whatever i changed

--- scripts/rps.php
+++ scripts/rps.php
@@ -5,7 +5,8 @@
 #####################################################################################################
 
 /*
-exec:~rps|10|0|0|1|*|PRIVMSG|||php scripts/rps.php %%trailing%% %%dest%% %%nick%% %%alias%% %%params%%
+#exec:~rps|10|0|0|1|*|PRIVMSG|||php scripts/rps.php %%trailing%% %%dest%% %%nick%% %%alias%% %%params%%
+exec:~rps|10|0|0|1||PRIVMSG|||php scripts/rps.php %%trailing%% %%dest%% %%nick%% %%alias%% %%params%%
 */
 
 #####################################################################################################
@@ -22,7 +23,8 @@ $data=get_array_bucket("<<EXEC_RPS_DATA>>");
 
 if (valid_rps_sequence($trailing)==True)
 {
-  $account=users_get_account($nick);
+  #$account=users_get_account($nick);
+  $account=$nick;
   if ($account=="")
   {
     return;
@@ -108,17 +110,106 @@ s = s
 
 function update_ranking(&$data)
 {
-  $out="rankings after ".$data["rounds"]." rounds:\n\n";
   foreach ($data["users"] as $account => $user_data)
   {
+    $data["users"][$account]["wins"]=0;
+    $data["users"][$account]["losses"]=0;
+    $data["users"][$account]["ties"]=0;
     for ($i=0;$i<strlen($data["users"][$account]["sequence"]);$i++)
     {
-      
+      foreach ($data["users"] as $sub_account => $sub_user_data)
+      {
+        if (isset($data["users"][$sub_account]["sequence"][$i])==True)
+        {
+          if ($sub_account==$account)
+          {
+            continue;
+          }
+          switch ($data["users"][$account]["sequence"][$i])
+          {
+            case "r":
+              switch ($data["users"][$sub_account]["sequence"][$i])
+              {
+                case "r":
+                  $data["users"][$account]["ties"]=$data["users"][$account]["ties"]+1;
+                  break;
+                case "p":
+                  $data["users"][$account]["losses"]=$data["users"][$account]["losses"]+1;
+                  break;
+                case "s":
+                  $data["users"][$account]["wins"]=$data["users"][$account]["wins"]+1;
+                  break;
+              }
+              break;
+            case "p":
+              switch ($data["users"][$sub_account]["sequence"][$i])
+              {
+                case "r":
+                  $data["users"][$account]["wins"]=$data["users"][$account]["wins"]+1;
+                  break;
+                case "p":
+                  $data["users"][$account]["ties"]=$data["users"][$account]["ties"]+1;
+                  break;
+                case "s":
+                  $data["users"][$account]["losses"]=$data["users"][$account]["losses"]+1;
+                  break;
+              }
+              break;
+            case "s":
+              switch ($data["users"][$sub_account]["sequence"][$i])
+              {
+                case "r":
+                  $data["users"][$account]["losses"]=$data["users"][$account]["losses"]+1;
+                  break;
+                case "p":
+                  $data["users"][$account]["wins"]=$data["users"][$account]["wins"]+1;
+                  break;
+                case "s":
+                  $data["users"][$account]["ties"]=$data["users"][$account]["ties"]+1;
+                  break;
+              }
+              break;
+          }
+        }
+      }
     }
+  }
+  $rankings=array();
+  foreach ($data["users"] as $account => $user_data)
+  {
     $data["users"][$account]["rank"]=0;
-    $out=$out.$account."\t".$data["users"][$account]["sequence"]."\t".$data["users"][$account]["rank"]."\n";
+    $rankings[$account]=$data["users"][$account]["losses"]-$data["users"][$account]["wins"];
+  }
+  uasort($rankings,"ranking_sort_callback");
+  $ranking_keys=array_keys($rankings);
+  foreach ($data["users"] as $account => $user_data)
+  {
+    $data["users"][$account]["rank"]=array_search($account,$ranking_keys);
+  }
+  $out="rankings after ".$data["rounds"]." rounds:\n\n";
+  $actlen=0;
+  foreach ($data["users"] as $account => $user_data)
+  {
+    if (strlen($account)>$actlen)
+    {
+      $actlen=strlen($account);
+    }
+  }
+  $head_account="account";
+  $head_sequence="sequence";
+  $out=$out.$head_account.str_repeat(" ",$actlen-strlen($head_account))."\t".$head_sequence.str_repeat(" ",$data["rounds"]-strlen($head_sequence))."\twins\tloss\tties\trank\n";
+  foreach ($data["users"] as $account => $user_data)
+  {
+    $out=$out.$account.str_repeat(" ",$actlen-strlen($account))."\t".$data["users"][$account]["sequence"].str_repeat(" ",$data["rounds"]-strlen($data["users"][$account]["sequence"]))."\t".$data["users"][$account]["wins"]."\t".$data["users"][$account]["losses"]."\t".$data["users"][$account]["ties"]."\t".$data["users"][$account]["rank"]."\n";
   }
   return $out;
+}
+
+#####################################################################################################
+
+function ranking_sort_callback($a,$b)
+{
+  return ($a-$b);
 }
 
 #####################################################################################################
