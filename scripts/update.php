c9adddfe6fa27ab531b732c27c28855cc943f9bd
crutchy <jared@debian>
2016-03-14 07:48:09 +1100

    whatever i changed

---
+++ scripts/update.php
@@ -0,0 +1,47 @@
+<?php
+
+#####################################################################################################
+
+/*
+#exec:~update|60|0|0|1|@||||php scripts/update.php %%trailing%% %%dest%% %%nick%% %%alias%% %%server%%
+*/
+
+#####################################################################################################
+
+require_once("lib.php");
+
+$trailing=trim($argv[1]);
+$dest=$argv[2];
+$nick=$argv[3];
+$alias=$argv[4];
+$server=$argv[5];
+
+if ($trailing=="")
+{
+  privmsg("syntax: ~update <filename> (operator only command)");
+  return;
+}
+
+$response=wget_ssl("raw.githubusercontent.com","/crutchy-/exec-irc-bot/master/".$trailing);
+if ($response=="")
+{
+  privmsg("error downloading file (1)");
+  return;
+}
+$headers=exec_get_headers($response);
+if ($headers===False)
+{
+  privmsg("error downloading file (2)");
+  return;
+}
+$lines=explode(PHP_EOL,$headers);
+if ((strpos($lines[0],"404")!==False) or (strpos($lines[0],"Error")!==False))
+{
+  privmsg("error downloading file (3)");
+  return;
+}
+$content=strip_headers($response);
+
+#####################################################################################################
+
+?>
