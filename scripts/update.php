213de2381b1829e1c223522ae1bf3ba15ca32c5c
crutchy <jared@debian>
2016-03-16 22:00:06 +1100

    whatever i changed

--- scripts/update.php
+++ scripts/update.php
@@ -22,7 +22,7 @@ switch ($alias)
   case "~update-exec-file":
     if ($trailing=="")
     {
-      privmsg("syntax: ~update <filename> (operator only command)");
+      privmsg("syntax: ~update-exec-file <filename> (operator only command)");
       return;
     }
     privmsg("attempting to download https://raw.githubusercontent.com/crutchy-/exec-irc-bot/master/".$trailing);
