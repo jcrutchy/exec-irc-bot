d146b91a307c23a6429ca31d1e09a6fcdf25a4b8
crutchy <jared@debian>
2016-04-18 08:03:48 +1000

    whatever i changed

--- scripts/sneak.php
+++ scripts/sneak.php
@@ -30,13 +30,16 @@ players are identified by their nick!user@hostname
 there are goody boxes randomly dispersed around the play area, that are consumed when occupied and appear randomly at unoccupied coordinates
 goody boxes could kill the player, relocate them to a random coordinate (if occupied resulting in a kill for that player), or could indicate the relative location of the nearest player (eg: 2 spaces up, 4 spaces right)
 
+output to players is via pm
+channel output to #sneak is only certain events (when someone dies or the map changes)
+
 */
 
 #####################################################################################################
 
 require_once("lib.php");
 
-$trailing=trim($argv[1]);
+$trailing=strtolower(trim($argv[1]));
 $dest=$argv[2];
 $nick=$argv[3];
 $user=$argv[4];
@@ -49,6 +52,8 @@ $server=$argv[9];
 $id="$nick!$user@$hostname";
 $serv=base64_encode($server);
 
+$save=False;
+
 $fn=DATA_PATH."sneak_data_$serv.txt";
 if (file_exists($fn)==True)
 {
@@ -57,16 +62,88 @@ if (file_exists($fn)==True)
 else
 {
   $data=array();
-  $data["goodies"]=array();
-  $data["players"]=array();
-  $data["map"]=array();
-  
   $save=True;
 }
 
-$save=False;
+$chan="#sneak";
 
+if (isset($data[$chan])==False)
+{
+  init_chan($chan);
+}
 
+$gm_accounts=array("crutchy");
+
+$parts=explode(" ",$trailing);
+if (count($parts)==2)
+{
+  $chan=array_shift($parts);
+  if (isset($data[$chan])==False)
+  {
+    pm($nick,"sneak: invalid game channel");
+    return;
+  }
+}
+
+if (count($parts)<>1)
+{
+  pm($nick,"sneak: invalid game command");
+  return;
+}
+
+if (isset($data["players"][$id])==False)
+{
+  $data["players"][$id]=array();
+  $save=True;
+}
+
+switch ($trailing)
+{
+  case "gm-del-chan":
+    if (is_gm($nick)==True)
+    {
+      if (isset($data[$chan])==True)
+      {
+        unset($data[$chan]);
+        $save=True;
+        pm($nick,"sneak: chan deleted");
+      }
+      else
+      {
+        pm($nick,"sneak: chan not found");
+      }
+    }
+    break;
+  case "gm-init-chan":
+    if (is_gm($nick)==True)
+    {
+      if (isset($data[$chan])==True)
+      {
+        unset($data[$chan]);
+      }
+      init_chan($chan);
+      pm($nick,"sneak: chan initialized");
+    }
+    break;
+  case "new":
+
+    break;
+  case "rank":
+
+    break;
+  case "l":
+
+    break;
+  case "r":
+
+    break;
+  case "u":
+
+    break;
+  case "d":
+
+    break;
+}
 
 if ($save==True)
 {
@@ -74,6 +151,39 @@ if ($save==True)
   {
     privmsg("error writing data file");
   }
+}
+
+#####################################################################################################
+
+function is_gm($nick)
+{
+  global $gm_accounts;
+  $account=users_get_account($nick);
+  if ($account=="")
+  {
+    return False;
+  }
+  if (in_array($account,$gm_accounts)==True)
+  {
+    return True;
+  }
+  else
+  {
+    return False;
+  }
+}
+
+#####################################################################################################
+
+function init_chan($chan)
+{
+  global $save;
+  global $data;
+  $data[$chan]=array();
+  $data[$chan]["players"]=array();
+  $data[$chan]["goodies"]=array();
+  $data[$chan]["map_size"]=30;
+  $save=True;
 }
 
 #####################################################################################################
