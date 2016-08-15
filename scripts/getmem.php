a9080441a650be4e4ea5112276249b67ec0a6e8d
crutchy <jared.crutchfield@hotmail.com>
2016-08-15 23:51:47 +1000

    whatever i changed

--- scripts/getmem.php
+++ scripts/getmem.php
@@ -15,7 +15,19 @@ require_once("lib.php");
 
 $trailing=trim($argv[1]);
 
-privmsg(get_bucket("<<BOT_MEMORY_USAGE>>"));
+#privmsg(get_bucket("<<BOT_MEMORY_USAGE>>"));
+
+$output=shell_exec("top -n1 -c -b | grep -m 1 'php irc.php'");
+$output=explode(" ",$output);
+delete_empty_elements($output);
+if (count($output)>=12)
+{
+  privmsg("RES: ".$output[5]);
+}
+else
+{
+  privmsg("something borked");
+}
 
 #####################################################################################################
 
